9bd256d3fce9929671a096c9022954b8acc80602
Santapan Sarkar <santapansarkar@gmail.com>
2021-09-18 23:32:10 +0530

    ec2 function modified

--- ec2.py
+++ ec2.py
@@ -2,6 +2,7 @@ from copy import Error
 import boto3
 import logging
 import argparse
+from botocore.exceptions import ClientError
 
 
 
@@ -11,10 +12,15 @@ def describe_ec2():
         logging.info("low level client object connected to AWS EC2")
         response = ec2.describe_instances()
         logging.info("Describe Instance API called")
-        ec2_id = response['Reservations'][0]['Instances'][0]['InstanceId']
-        logging.info(f'Details of ec2 instance id --> {ec2_id}')
-        return ec2_id
-    except Exception as e:
+        for rec in response['Reservations']:
+            for instance in rec['Instances']:
+                compute_type = instance['InstanceType']
+                if compute_type != 't2.micro':
+                    logging.warning(f'Current instance --> {instance["InstanceId"]} is outside limit of allowed compute capacity, hence terminating')    
+                    stop_ec2(instance['InstanceId'])    
+                logging.info(f'Current instance --> {instance["InstanceId"]} is within limit of allowed compute capacity')    
+        #logging.info(f'Details of ec2 instance id --> {rec}')
+    except ClientError as e:
         logging.error('issue in describing ec2 instances'.format(e))    
 
     
@@ -23,8 +29,8 @@ def stop_ec2(instance_id):
     try:
         ec2 = boto3.client('ec2')
         ec2.stop_instances(InstanceIds=[instance_id])    
-    except Exception as e:
-        logging.error('issue while stopping ec2'.format(e))
+    except ClientError as e:
+        logging.error(f'issue while stopping ec2 --> {instance_id}'.format(e))
             
 
 def start_ec2(instance_id):
@@ -32,7 +38,7 @@ def start_ec2(instance_id):
         ec2 = boto3.client('ec2')
         ec2.start_instances(InstanceIds=[instance_id], DryRun=False)
     except Exception as e:
-        logging.error('issue while starting ec2'.format(e))
+        logging.error('issue while starting ec2 '.format(e))
 
 
 
@@ -56,8 +62,9 @@ def scale_in_out(**kwargs):
         asg = boto3.client('autoscaling')
         response = asg.set_desired_capacity(**kwargs)  
         logging.info(f'desired capacity updated successfully for auto scaling group {args._get_args}')      
-    except Exception as e:
-        logging.error('Error while scale in/out activity'.format(e))    
+    except ClientError as e:
+        logging.error('Error while scale in/out activity {}'.format(e))    
+        
     
 
 def log_level(**kwargs):
@@ -75,7 +82,8 @@ if __name__=='__main__':
             'filename':'ec2.log',
             'format':'%(asctime)s - %(process)d - %(levelname)s - %(message)s',
             'level':'INFO'      
-            }    
+            }   
+    '''         
     parser = argparse.ArgumentParser(description="Choose method of operation")
 
     # Add each of the arguments using the parser.add_argument() method
@@ -88,6 +96,7 @@ if __name__=='__main__':
     )
     
     # This will inspect the command line, convert each argument to the appropriate type and then invoke the appropriate action.
+    
     args = parser.parse_args()
     input = {**vars(args)}
     if input['FunctionName'] == 'stop_ec2':
@@ -124,15 +133,34 @@ if __name__=='__main__':
         #print('call scale_in_out')
     else:
         print(input['FunctionName']) 
-
+    '''
 
 
     
 
-    #log_level(**log_cfg)
-    #ec2_id = describe_ec2()    
+    log_level(**log_cfg)
+    #describe_ec2()    
     #stop_ec2(ec2_id)
     #start_ec2(ec2_id)
     #describe_asg()
-    #scale_in_out()
+    
+    parser = argparse.ArgumentParser(description= 'use help switch to know more')
+    parser.add_argument(
+                          '--asgname',
+                          dest='AutoScalingGroupName',
+                          type=str,
+                          help='AutoScalingGroupName',
+                          required=True
+                        ) 
+
+    parser.add_argument(
+                        '--capacity',
+                        dest="DesiredCapacity",
+                        type=int,
+                        help="desired number of compute resources",
+                        required=True
+                        )
+    args = parser.parse_args()
+    scale_in_out(**vars(args))
+    
    
